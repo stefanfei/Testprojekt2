282e3497f38140eda5027cbbaba443a81bf34dc2
Stefan Feirer <stefan.feirer@gmx.at>
2012-01-26 15:02:25 +0100

    neu commited

--- src/stefan/test/TestActivity.java
+++ src/stefan/test/TestActivity.java
@@ -10,5 +10,7 @@ public class TestActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        
     }
 }
